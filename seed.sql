3db68410f3acf99d44b943eab863cab8a9a3a849
Malek <engmalek.civil@gmail.com>
2021-03-01 23:50:17 +0200

    preFinal

--- seed.sql
+++ seed.sql
@@ -5,5 +5,7 @@ CREATE TABLE books(
     img_url VARCHAR(255),
     title VARCHAR(255),
     author VARCHAR(255),
-    descriptions TEXT
+    descriptions TEXT,
+    isbn VARCHAR(255),
+    bookShelf VARCHAR(255)
 );
